882dc45f744a4205ed9b514f311ac53db521a102
Sam Reid <reids@colorado.edu>
2014-07-29 12:22:56 -0600

    Ported static fields and code from PhotonAbsorptionModel.java

--- js/MoleculesAndLightScreen.js
+++ js/MoleculesAndLightScreen.js
@@ -10,7 +10,7 @@ define( function( require ) {
   'use strict';
 
   // modules
-  var MoleculesAndLightModel = require( 'MOLECULES_AND_LIGHT/model/MoleculesAndLightModel' );
+  var PhotonAbsorptionModel = require( 'MOLECULES_AND_LIGHT/photonabsorption/PhotonAbsorptionModel' );
   var MoleculesAndLightScreenView = require( 'MOLECULES_AND_LIGHT/view/MoleculesAndLightScreenView' );
   var inherit = require( 'PHET_CORE/inherit' );
   var Screen = require( 'JOIST/Screen' );
@@ -20,7 +20,7 @@ define( function( require ) {
 
   function MoleculesAndLightScreen() {
     Screen.call( this, titleString, null /* no icon, single-screen sim */,
-      function() { return new MoleculesAndLightModel(); },
+      function() { return new PhotonAbsorptionModel(); },
       function( model ) { return new MoleculesAndLightScreenView( model ); },
       { backgroundColor: 'black' }
     );
---
+++ js/photonabsorption/WavelengthConstants.js
@@ -0,0 +1,20 @@
+// Copyright 2002-2013, University of Colorado Boulder
+
+/**
+ * Wavelength Constants, taken from photon-absorption/WavelengthConstants.java
+ *
+ * @author Jesse Greenberg (Phet Interactive Simulations)
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
+define( function() {
+  'use strict';
+
+  return {
+    SUNLIGHT_WAVELENGTH: 400E-9,
+    MICRO_WAVELENGTH: 20,
+    IR_WAVELENGTH: 850E-9,
+    VISIBLE_WAVELENGTH: 580E-9,
+    UV_WAVELENGTH: 100E-9,
+    DEBUG_WAVELENGTH: 1
+  };
+} );
