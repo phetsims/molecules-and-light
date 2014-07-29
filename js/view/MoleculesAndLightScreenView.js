e99436424bc67bd075fb948fa85ff50366b6030a
Sam Reid <reids@colorado.edu>
2014-07-29 12:47:41 -0600

    Started porting the control panel

--- js/view/MoleculesAndLightScreenView.js
+++ js/view/MoleculesAndLightScreenView.js
@@ -14,21 +14,23 @@ define( function( require ) {
   var inherit = require( 'PHET_CORE/inherit' );
   var ScreenView = require( 'JOIST/ScreenView' );
   var Image = require( 'SCENERY/nodes/Image' );
+  var QuadEmissionFrequencyControlPanel = require( 'MOLECULES_AND_LIGHT/view/QuadEmissionFrequencyControlPanel' );
 
   // images
   var heatLampImage = require( 'image!MOLECULES_AND_LIGHT/heat-lamp.png' );
 
   /**
-   * @param {BarMagnetModel} model
+   * @param {BarMagnetModel} photonAbsorptionModel
    * @constructor
    */
-  function MoleculesAndLightScreenView( model ) {
+  function MoleculesAndLightScreenView( photonAbsorptionModel ) {
 
     ScreenView.call( this );
 
     // Add the heat lamp to the left center of screen
-    this.addChild( new Image( heatLampImage, { left: 0, centerY: this.layoutBounds.centerY, scale: 0.75 } ) );
-
+    var heatLampNode = new Image( heatLampImage, { left: 0, centerY: this.layoutBounds.centerY, scale: 0.75 } );
+    this.addChild( heatLampNode );
+    this.addChild( new QuadEmissionFrequencyControlPanel( photonAbsorptionModel, {top: heatLampNode.bottom + 100, left: 20} ) );
   }
 
   return inherit( ScreenView, MoleculesAndLightScreenView );
