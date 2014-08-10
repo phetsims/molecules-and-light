e8c000ddc670f157d27f72a501a3116f18838fe1
Jesse Greenberg <jesse.greenberg@colorado.edu>
2014-08-10 14:21:06 -0600

    Ported the OxygenAtom model

---
+++ js/photonabsorption/model/atoms/OxygenAtom.js
@@ -0,0 +1,52 @@
+// Copyright 2002-2014, University of Colorado
+
+/**
+ * Class that represents an atom of Oxygen in the model.
+ *
+ * @author John Blanco
+ * @author Jesse Greenberg
+ */
+
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var inherit = require( 'PHET_CORE/inherit' );
+  var Color = require( 'SCENERY/util/Color' );
+  var Vector2 = require( 'DOT/Vector2' );
+  var Atom = require( 'MOLECULES_AND_LIGHT/photonabsorption/model/atoms/Atom' );
+
+  // Model data for the Oxygen atom.
+  // TODO: The color below is called RED_COLORBLIND and was taken from PhetColorScheme.java.  Do we have such color schemes in the JavaScript scenery?
+  var REPRESENTATION_COLOR = new Color( 255, 85, 0 ); //Reddish color that also looks good in colorblind tests, see #2753
+  var MASS = 12.011;   // In atomic mass units (AMU).
+  var RADIUS = 73;     // In picometers.
+
+  /**
+   * Constructor for an Oxygen atom.  There is an optional Vector2 parameter
+   * which specifies the location of this Oxygen atom.
+   *
+   * @param {*} options - Optional Vector2 for this Oxygen atom's location.
+   * @constructor
+   */
+  function OxygenAtom( options ) {
+    // Supertype constructor
+    Atom.call( this, options );
+
+    // Options extension for a possible input vector.
+    options = _.extend( {
+      // defaults
+      position: new Vector2( 0, 0 )
+    }, options );
+    this.options = options;
+
+    this.representationColor = REPRESENTATION_COLOR;
+    this.radius = RADIUS;
+    this.mass = MASS;
+    this.position = options.position;
+
+  }
+
+  return inherit( Atom, OxygenAtom )
+
+} );
