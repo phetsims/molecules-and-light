5c2b99e5c602bf1c0cec50df6ea80abf110b26f7
Jesse Greenberg <jesse.greenberg@colorado.edu>
2014-08-13 10:57:42 -0600

    Port of constituent O 'molecule' as a model to be used in break apart strategies

---
+++ js/photonabsorption/model/molecules/O.js
@@ -0,0 +1,61 @@
+// Copyright 2002-2014, University of Colorado
+
+/**
+ * Class that represents a single atom of oxygen in the model.  I hate to name
+ * a class "O", but it is necessary for consistency with other molecules
+ * names.
+ *
+ * @author John Blanco
+ * @author Jesse Greenberg
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var inherit = require( 'PHET_CORE/inherit' );
+  var Vector2 = require( 'DOT/Vector2' );
+  var Molecule = require( 'MOLECULES_AND_LIGHT/photonabsorption/model/Molecule' );
+  var OxygenAtom = require( 'MOLECULES_AND_LIGHT/photonabsorption/model/atoms/OxygenAtom' )
+
+  function O( options ) {
+    // Supertype constructor
+    Molecule.call( this );
+
+    // Options extension for a possible input vector.
+    options = _.extend( {
+      // defaults
+      initialCenterOfGravityPos: new Vector2( 0, 0 )
+    }, options );
+    this.options = options;
+
+    // Instance Data
+    this.oxygenAtom = new OxygenAtom();
+    this.initialCenterOfGravityPos = options.initialCenterOfGravityPos;
+
+    // Configure the base class.
+    this.addAtom( this.oxygenAtom );
+
+    // Set up the photon wavelengths to absorb.
+
+    // Set the initial offsets.
+    this.initializeAtomOffsets();
+
+    // Set the initial center of gravity position.;
+    this.setCenterOfGravityPosVec( this.initialCenterOfGravityPos );
+
+  }
+
+  return inherit( Molecule, O, {
+
+    /**
+     * Initialize and set the COG offsets for the position of this Oxygen atom.
+     */
+    initializeAtomOffsets: function() {
+      this.addInitialAtomCogOffset( this.oxygenAtom, new Vector2( 0, 0 ) );
+
+      this.updateAtomPositions();
+    }
+
+  } )
+
+} );
