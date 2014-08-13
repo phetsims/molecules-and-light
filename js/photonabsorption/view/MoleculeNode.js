c38cfcfafdf9eb6f265c74c25e88649e2c215159
Jesse Greenberg <jesse.greenberg@colorado.edu>
2014-08-13 13:22:20 -0600

    Implement event trigger on electronic energy state change

--- js/photonabsorption/view/MoleculeNode.js
+++ js/photonabsorption/view/MoleculeNode.js
@@ -37,57 +37,37 @@ define( function( require ) {
 
     // Add the atoms which compose this molecule to the atomLayer
     // TODO: requires the AtomNode.js dependency file.
-    for( var atom = 0; atom < molecule.getAtoms().length; atom++ ) {
+    for ( var atom = 0; atom < molecule.getAtoms().length; atom++ ) {
       thisNode.atomLayer.addChild( new AtomNode( atom, mvt ) );
     }
 
     // Add the atomic bonds which form the structure of this molecule to the bondLayer
     // TODO: requires the AtomicBondNode.js dependency file.
-    for( var atomicBond = 0; atomicBond < molecule.getAtomicBonds().length; atomicBond++ ) {
-      thisNode.bondLayer.addChild( new AtomicBondNode ( atomicBond, mvt ) );
+    for ( var atomicBond = 0; atomicBond < molecule.getAtomicBonds().length; atomicBond++ ) {
+      thisNode.bondLayer.addChild( new AtomicBondNode( atomicBond, mvt ) );
     }
 
+    molecule.on( 'electronicEnergyStateChanged', function( molecule ) {
+      for ( var i = 0; i < thisNode.atomLayer.children.length; i++ ) {
+        var atomNode = thisNode.atomLayer.getChildAt( i );
+        atomNode.setHighlighted( molecule.isHighElectronicEnergyState() );
+      }
+
+    } );
+    // Make sure the highlighting is correct when the simulation starts.
+    molecule.trigger( 'electronicEnergyStateChanged' );
   }
 
-  return inherit( Node, MoleculeNode, {
-
-
-  } )
+  return inherit( Node, MoleculeNode );
 
 } );
 
 //
-
 //
 //  //------------------------------------------------------------------------
 //  // Constructor(s)
 //  //------------------------------------------------------------------------
 //
-//  public MoleculeNode( final Molecule molecule, ModelViewTransform2D mvt ) {
-//    bondLayer = new PNode();
-//    addChild( bondLayer );
-//    atomLayer = new PNode();
-//    addChild( atomLayer );
-//
-//    for ( Atom atom : molecule.getAtoms() ) {
-//      atomLayer.addChild( new AtomNode( atom, mvt ) );
-//    }
-//
-//    for ( AtomicBond atomicBond : molecule.getAtomicBonds() ) {
-//      bondLayer.addChild( new AtomicBondNode( atomicBond, mvt ) );
-//    }
-//    final Molecule.Adapter listener = new Molecule.Adapter() {
-//      @Override
-//      public void electronicEnergyStateChanged( Molecule molecule ) {
-//        super.electronicEnergyStateChanged( molecule );
-//        for ( int i = 0; i < atomLayer.getChildrenCount(); i++ ) {
-//          AtomNode atomNode = (AtomNode) atomLayer.getChild( i );
-//          atomNode.setHighlighted( molecule.isHighElectronicEnergyState() );
-//        }
-//      }
-//    };
-//    molecule.addListener( listener );
-//    listener.electronicEnergyStateChanged( molecule );
 //
 //    // If enabled, show the center of gravity of the molecule.
 //    if ( SHOW_COG ) {
