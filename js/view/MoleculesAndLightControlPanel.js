2d5d51a495a946575ae3274106f1baf7bb20c2bf
Jesse Greenberg <jesse.greenberg@colorado.edu>
2014-10-23 15:13:19 -0600

    Reduced negative space between molecule radio buttons

--- js/view/MoleculesAndLightControlPanel.js
+++ js/view/MoleculesAndLightControlPanel.js
@@ -68,7 +68,7 @@ define( function( require ) {
 
     var radioButtons = new RadioButtonGroup( model.photonTargetProperty, radioButtonContent,
       {
-        spacing: 7,
+        spacing: 1,
         baseColor: 'black',
         buttonContentYMargin: 7,
         selectedStroke: 'white',
