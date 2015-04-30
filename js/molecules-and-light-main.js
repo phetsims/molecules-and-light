53cce69ff6e30fa082e509b7c8d1f0e9af76ac65
Sam Reid <reids@colorado.edu>
2015-04-29 23:30:51 -0600

    Fixed tandem support for joist, see #112

--- js/molecules-and-light-main.js
+++ js/molecules-and-light-main.js
@@ -12,9 +12,12 @@ define( function( require ) {
   var MoleculesAndLightScreen = require( 'MOLECULES_AND_LIGHT/moleculesandlight/MoleculesAndLightScreen' );
   var Sim = require( 'JOIST/Sim' );
   var SimLauncher = require( 'JOIST/SimLauncher' );
+  var Tandem = require( 'TANDEM/Tandem' );
 
   // strings
   var simTitle = require( 'string!MOLECULES_AND_LIGHT/molecules-and-light.name' );
+
+  var tandem = new Tandem();
 
   var simOptions = {
     credits: {
@@ -23,7 +26,8 @@ define( function( require ) {
       team: 'Yuen-ying Carpenter, Trish Loeblein, Emily Moore, Robert Parson, Ariel Paul, Kathy Perkins',
       qualityAssurance: 'Steele Dalton, Oliver Orejola, Bryan Yoelin',
       graphicArts: 'Alex Klinger'
-    }
+    },
+    tandem: tandem
   };
 
   // Appending '?dev' to the URL will enable developer-only features.
@@ -34,7 +38,7 @@ define( function( require ) {
   }
 
   SimLauncher.launch( function() {
-    var sim = new Sim( simTitle, [ new MoleculesAndLightScreen() ], simOptions );
+    var sim = new Sim( simTitle, [ new MoleculesAndLightScreen( tandem.createTandem( 'moleculesAndLightScreen' ) ) ], simOptions );
     sim.start();
   } );
 } );
