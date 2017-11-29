67c151893ebbd7af2278111d87c06c95e886cf63
zepumph <michael.kauzmann@colorado.edu>
2017-11-28 16:36:45 -0900

    add value update, https://github.com/phetsims/a11y-research/issues/65

--- js/common/MoleculesAndLightA11yStrings.js
+++ js/common/MoleculesAndLightA11yStrings.js
@@ -21,7 +21,7 @@ define( function( require ) {
 
   if ( phet.chipper.queryParameters.stringTest === 'xss' ) {
     for ( var key in MoleculesAndLightA11yStrings ) {
-      MoleculesAndLightA11yStrings[ key ] += '<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQIW2NkYGD4DwABCQEBtxmN7wAAAABJRU5ErkJggg==" onload="window.location.href=atob(\'aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1kUXc0dzlXZ1hjUQ==\')" />';
+      MoleculesAndLightA11yStrings[ key ].value += '<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQIW2NkYGD4DwABCQEBtxmN7wAAAABJRU5ErkJggg==" onload="window.location.href=atob(\'aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1kUXc0dzlXZ1hjUQ==\')" />';
     }
   }
 
