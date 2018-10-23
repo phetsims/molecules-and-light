21c56a4f935bbddc08fa3b7788f0a7a1fae4fe91
Chris Malley <cmalley@pixelzoom.com>
2018-10-23 11:36:47 -0600

    move IO types to modules comment block, https://github.com/phetsims/phet-io/issues/1396
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/photon-absorption/model/PhotonIO.js
+++ js/photon-absorption/model/PhotonIO.js
@@ -11,11 +11,11 @@ define( function( require ) {
 
   // modules
   var moleculesAndLight = require( 'MOLECULES_AND_LIGHT/moleculesAndLight' );
-
-  // ifphetio
-  var assertInstanceOf = require( 'ifphetio!PHET_IO/assertInstanceOf' );
   var NumberIO = require( 'TANDEM/types/NumberIO' );
   var ObjectIO = require( 'TANDEM/types/ObjectIO' );
+  
+  // ifphetio
+  var assertInstanceOf = require( 'ifphetio!PHET_IO/assertInstanceOf' );
   var phetioInherit = require( 'ifphetio!PHET_IO/phetioInherit' );
 
   /**
