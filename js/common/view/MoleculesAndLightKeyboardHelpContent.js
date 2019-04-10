35351fef7e130343af44ed02ee532ed372406680
Jesse Greenberg <jesse.greenberg@colorado.edu>
2019-04-10 16:48:44 -0600

    use general type for slider and general keyboard help content, see phetsims/scenery-phet#483

--- js/common/view/MoleculesAndLightKeyboardHelpContent.js
+++ js/common/view/MoleculesAndLightKeyboardHelpContent.js
@@ -9,34 +9,25 @@ define( require => {
   'use strict';
 
   // modules
-  const GeneralKeyboardHelpSection = require( 'SCENERY_PHET/keyboard/help/GeneralKeyboardHelpSection' );
   const moleculesAndLight = require( 'MOLECULES_AND_LIGHT/moleculesAndLight' );
-  const SliderKeyboardHelpSection = require( 'SCENERY_PHET/keyboard/help/SliderKeyboardHelpSection' );
-  const TwoColumnKeyboardHelpContent = require( 'SCENERY_PHET/keyboard/help/TwoColumnKeyboardHelpContent' );
+  const SliderAndGeneralKeyboardHelpContent = require( 'SCENERY_PHET/keyboard/help/SliderAndGeneralKeyboardHelpContent' );
 
   // strings
   const keyboardHelpDialogLightSourceSliderString = require( 'string!MOLECULES_AND_LIGHT/KeyboardHelpDialog.lightSourceSlider' );
 
-  // constants
-  const HELP_SECTION_OPTIONS = {
-    labelMaxWidth: 160 // i18n, a bit shorter than default so general and slider sections fits side by side
-  };
-
-  class MoleculesAndLightKeyboardHelpContent extends TwoColumnKeyboardHelpContent {
+  class MoleculesAndLightKeyboardHelpContent extends SliderAndGeneralKeyboardHelpContent {
 
     /**
      * @param {tandem} tandem
      */
     constructor( tandem ) {
-
-      const sliderHelpSection = new SliderKeyboardHelpSection( _.extend( HELP_SECTION_OPTIONS, {
-        headingString: keyboardHelpDialogLightSourceSliderString
-      } ) );
-      const generalNavigationHelpSection = new GeneralKeyboardHelpSection( _.extend( HELP_SECTION_OPTIONS, {
-        withGroupContent: true
-      } ) );
-
-      super( sliderHelpSection, generalNavigationHelpSection, {
+      super( {
+        sliderSectionOptions: {
+          headingString: keyboardHelpDialogLightSourceSliderString
+        },
+        generalSectionOptions: {
+          withGroupContent: true
+        },
         tandem: tandem
       } );
     }
