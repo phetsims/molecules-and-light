8481f1007b6c2c8b1a94d841e374a25e7ca6c389
zepumph <michael.kauzmann@colorado.edu>
2019-05-29 11:31:50 -0800

    update baseline files, https://github.com/phetsims/phet-io/issues/1466

--- js/phet-io/molecules-and-light-phet-io-elements-baseline.js
+++ js/phet-io/molecules-and-light-phet-io-elements-baseline.js
@@ -3414,7 +3414,7 @@ window.phet.phetio.phetioElementsBaseline =
     "moleculesAndLight.navigationBar.phetButton.pickableProperty": {
       "phetioDocumentation": "Set whether the phetButton will be pickable (and hence interactive), see the NodeIO documentation for more details",
       "phetioEventType": "model",
-      "phetioFeatured": false,
+      "phetioFeatured": true,
       "phetioHighFrequency": false,
       "phetioPlayback": false,
       "phetioReadOnly": false,
