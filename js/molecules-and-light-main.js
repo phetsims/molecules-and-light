a88823d7e2802cb91239fd8eff92faf444800e06
Jesse <jesse.greenberg@colorado.edu>
2019-11-08 12:57:42 -0500

    () => return is bad text, see phetsims/energy-skate-park#140

--- js/molecules-and-light-main.js
+++ js/molecules-and-light-main.js
@@ -26,7 +26,7 @@ define( require => {
     keyboardHelpNode: keyboardHelpContent,
 
     createOptionsDialogContent: phet.chipper.queryParameters.supportsSound ?
-                                () => { return malSoundOptionsDialogContent.getContent(); } :
+                                () => { malSoundOptionsDialogContent.getContent(); } :
                                 null,
 
     credits: {
