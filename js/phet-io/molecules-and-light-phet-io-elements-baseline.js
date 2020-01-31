41a8d084fbe281b832c3a2b85bc47eb065b8f44e
samreid <reids@colorado.edu>
2020-01-31 08:51:12 -0700

    Update slider enabledRange phetioDocumentation, see https://github.com/phetsims/gravity-and-orbits/issues/293

--- js/phet-io/molecules-and-light-phet-io-elements-baseline.js
+++ js/phet-io/molecules-and-light-phet-io-elements-baseline.js
@@ -3200,7 +3200,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioTypeName": "PropertyIO<BooleanIO>"
     },
     "moleculesAndLight.moleculesAndLightScreen.view.observationWindow.photonEmitterNode.slider.enabledRangeProperty": {
-      "phetioDocumentation": "",
+      "phetioDocumentation": "Sliders support two ranges: the outer range which specifies the min and max of the track and the enabledRangeProperty, which determines how low and high the thumb can be dragged within the track.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
       "phetioFeatured": false,
