bf314a8d19208d5f10c6218512061f25626bf9ae
jbphet <john.blanco@colorado.edu>
2020-03-03 11:28:33 -0700

    spatialized rotation sounds, see #284

---
+++ js/common/MALConstants.js
@@ -0,0 +1,16 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * MALConstants is a collection of constants used throughout this simulation.
+ *
+ * @author John Blanco (PhET Interactive Simulations)
+ */
+
+import moleculesAndLight from '../moleculesAndLight.js';
+
+const MALConstants = {
+  USE_SPATIALIZED_SOUNDS: true
+};
+
+moleculesAndLight.register( 'MALConstants', MALConstants );
+export default MALConstants;
