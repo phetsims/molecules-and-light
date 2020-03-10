b83f77af1d73f28512e316df473aa03bb325dc1b
Jesse <jesse.greenberg@colorado.edu>
2020-03-10 14:52:11 -0400

    query parameters for emission frequency and slow play speed, see #295

---
+++ js/common/MoleculesAndLightQueryParameters.js
@@ -0,0 +1,37 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * Query parameters for molecules-and-light.
+ *
+ * @author Jesse Greenberg
+ */
+
+import moleculesAndLight from '../moleculesAndLight.js';
+
+const MoleculesAndLightQueryParameters = QueryStringMachine.getAll( {
+
+  // For testing, as we decide when the best emission frequency is for the "on" photon emitter. 0 has the emitter
+  // completely off, 1 has the emission frequency match fastest rate at published version of the sim.
+  emissionFrequency: {
+    type: 'number',
+    defaultValue: 0.85,
+    isValidValue: value => {
+      return 0 <= value && value <= 1;
+    }
+  },
+
+  // For testing as we decide how fast sim should play at the "slow" setting. This value is a proportion of the "normal"
+  // speed, so default 0.3 means slow time moves 30% as fast as fast "normal" time.
+  slowSpeed: {
+    type: 'number',
+    defaultValue: 0.3,
+    isValidValue: value => {
+      return 0 <= value && value <= 1;
+    }
+  }
+
+} );
+
+moleculesAndLight.register( 'MoleculesAndLightQueryParameters', MoleculesAndLightQueryParameters );
+
+export default MoleculesAndLightQueryParameters;
