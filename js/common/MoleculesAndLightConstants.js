0f9c9ec182c196fbc97b2122473346bf11fce7ff
Jesse <jesse.greenberg@colorado.edu>
2020-03-10 15:07:05 -0400

    rename MALConstants -> MoleculesAndLightConstants, see #297

---
+++ js/common/MoleculesAndLightConstants.js
@@ -0,0 +1,16 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * MoleculesAndLightConstants is a collection of constants used throughout this simulation.
+ *
+ * @author John Blanco (PhET Interactive Simulations)
+ */
+
+import moleculesAndLight from '../moleculesAndLight.js';
+
+const MoleculesAndLightConstants = {
+  USE_SPATIALIZED_SOUNDS: false
+};
+
+moleculesAndLight.register( 'MoleculesAndLightConstants', MoleculesAndLightConstants );
+export default MoleculesAndLightConstants;
