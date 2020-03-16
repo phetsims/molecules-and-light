7b4eb0a17484f9535d64867c688adff8261fa58c
zepumph <michael.kauzmann@colorado.edu>
2020-03-16 14:41:47 -0800

    remove ResetAllButtonIO.js, https://github.com/phetsims/scenery-phet/issues/572

--- js/phet-io/molecules-and-light-phet-io-elements-baseline.js
+++ js/phet-io/molecules-and-light-phet-io-elements-baseline.js
@@ -4276,7 +4276,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioReadOnly": false,
       "phetioState": false,
       "phetioStudioControl": true,
-      "phetioTypeName": "ResetAllButtonIO"
+      "phetioTypeName": "NodeIO"
     },
     "moleculesAndLight.moleculesAndLightScreen.view.resetAllButton.enabledProperty": {
       "phetioDocumentation": "When disabled, the button is grayed out and cannot be pressed",
