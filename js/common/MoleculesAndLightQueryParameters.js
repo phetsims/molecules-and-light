e8e07e0dc7e97f9b7f2d8ca2c553d71088eede73
Jesse <jesse.greenberg@colorado.edu>
2020-03-24 19:14:44 -0400

    PDOM content for the photon emitter button, see #296

--- js/common/MoleculesAndLightQueryParameters.js
+++ js/common/MoleculesAndLightQueryParameters.js
@@ -29,7 +29,12 @@ const MoleculesAndLightQueryParameters = QueryStringMachine.getAll( {
     isValidValue: value => {
       return 0 <= value && value <= 1;
     }
-  }
+  },
+
+  // For testing, as we decide what kind of PDOM representation the "On" button for the Photon Emitter
+  // should have. If included, it will be represented as a "switch" rather than a push button, see
+  // https://github.com/phetsims/molecules-and-light/issues/296
+  switch: { type: 'flag' }
 } );
 
 moleculesAndLight.register( 'MoleculesAndLightQueryParameters', MoleculesAndLightQueryParameters );
