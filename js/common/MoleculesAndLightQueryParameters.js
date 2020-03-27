f350913a2c62e9142b180b5ea99777f5e8bf1aec
Jesse <jesse.greenberg@colorado.edu>
2020-03-27 11:14:37 -0400

    Add way to test disabling PlayPauseNode, see #320

--- js/common/MoleculesAndLightQueryParameters.js
+++ js/common/MoleculesAndLightQueryParameters.js
@@ -34,7 +34,11 @@ const MoleculesAndLightQueryParameters = QueryStringMachine.getAll( {
   // For testing, as we decide what kind of PDOM representation the "On" button for the Photon Emitter
   // should have. If included, it will be represented as a "switch" rather than a push button, see
   // https://github.com/phetsims/molecules-and-light/issues/296
-  switch: { type: 'flag' }
+  switch: { type: 'flag' },
+
+  // For testing to decide on whether or not to disable the PlayPauseButton when the photon emitter is off.
+  // See https://github.com/phetsims/molecules-and-light/issues/320
+  disablePlayPause: { type: 'flag' }
 } );
 
 moleculesAndLight.register( 'MoleculesAndLightQueryParameters', MoleculesAndLightQueryParameters );
