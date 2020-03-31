393f9fc21e368c738b0685c7c6b5ba10854069a0
Jesse <jesse.greenberg@colorado.edu>
2020-03-31 17:25:03 -0400

    remove query param for slow speed factor, see #295

--- js/common/MoleculesAndLightQueryParameters.js
+++ js/common/MoleculesAndLightQueryParameters.js
@@ -10,17 +10,6 @@ import moleculesAndLight from '../moleculesAndLight.js';
 
 const MoleculesAndLightQueryParameters = QueryStringMachine.getAll( {
 
-  // For testing as we decide how fast sim should play at the "slow" setting. This value is a proportion of the "normal"
-  // speed, so default 0.3 means slow time moves 30% as fast as fast "normal" time.
-  // See https://github.com/phetsims/molecules-and-light/issues/295
-  slowSpeed: {
-    type: 'number',
-    defaultValue: 0.3,
-    isValidValue: value => {
-      return 0 <= value && value <= 1;
-    }
-  },
-
   // For testing, as we decide what kind of PDOM representation the "On" button for the Photon Emitter
   // should have. If included, it will be represented as a "switch" rather than a push button, see
   // https://github.com/phetsims/molecules-and-light/issues/296
