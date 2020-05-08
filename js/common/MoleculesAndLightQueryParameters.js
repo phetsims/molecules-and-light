b88f9574d2824c6cc874fde512b8643595f4b17c
Jesse <jesse.greenberg@colorado.edu>
2020-05-08 18:30:41 -0400

    Remove test query parameter for global hotkey, see #336

--- js/common/MoleculesAndLightQueryParameters.js
+++ js/common/MoleculesAndLightQueryParameters.js
@@ -12,14 +12,7 @@ const MoleculesAndLightQueryParameters = QueryStringMachine.getAll( {
 
   // For testing to decide on whether or not to disable the PlayPauseButton when the photon emitter is off.
   // See https://github.com/phetsims/molecules-and-light/issues/320
-  disablePlayPause: { type: 'flag' },
-
-  // For testing to decide if we will have a different time step for the keyboard command to move forward in time,
-  // see https://github.com/phetsims/molecules-and-light/issues/336
-  keyboardTimeStep: {
-    type: 'number',
-    defaultValue: 1 / 60
-  }
+  disablePlayPause: { type: 'flag' }
 } );
 
 moleculesAndLight.register( 'MoleculesAndLightQueryParameters', MoleculesAndLightQueryParameters );
