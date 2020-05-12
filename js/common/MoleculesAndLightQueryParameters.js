5042bcb9cdc8cc58b871f6c697d8918d4672c92c
Jesse <jesse.greenberg@colorado.edu>
2020-05-12 16:41:42 -0400

    alert every few pass throughs when a molecule/photon has no absorption strategy, see #333

--- js/common/MoleculesAndLightQueryParameters.js
+++ js/common/MoleculesAndLightQueryParameters.js
@@ -12,7 +12,12 @@ const MoleculesAndLightQueryParameters = QueryStringMachine.getAll( {
 
   // For testing to decide on whether or not to disable the PlayPauseButton when the photon emitter is off.
   // See https://github.com/phetsims/molecules-and-light/issues/320
-  disablePlayPause: { type: 'flag' }
+  disablePlayPause: { type: 'flag' },
+
+  // For testing description output - in the case where a photon/molecule pair has no absorption strategy,
+  // this is the number of "pass through" events to announce before letting the user know that no absorption
+  // is occurring. See https://github.com/phetsims/molecules-and-light/issues/333
+  passThroughCount: { type: 'number', defaultValue: 5 }
 } );
 
 moleculesAndLight.register( 'MoleculesAndLightQueryParameters', MoleculesAndLightQueryParameters );
