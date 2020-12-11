f17e507fc0bf21f69a909051e9cd8d72d16f25c6
Jesse <jesse.greenberg@colorado.edu>
2020-12-11 16:52:55 -0500

    Modifications for Open Sci Ed, see phetsims/special-ops#175

--- js/common/MoleculesAndLightQueryParameters.js
+++ js/common/MoleculesAndLightQueryParameters.js
@@ -10,6 +10,9 @@ import moleculesAndLight from '../moleculesAndLight.js';
 
 const MoleculesAndLightQueryParameters = QueryStringMachine.getAll( {
 
+  // whether or not to run with customizations for Open Sci Ed
+  openSciEd: { type: 'flag' },
+
   // For testing to decide on whether or not to disable the PlayPauseButton when the photon emitter is off.
   // See https://github.com/phetsims/molecules-and-light/issues/320
   disablePlayPause: { type: 'flag' }
