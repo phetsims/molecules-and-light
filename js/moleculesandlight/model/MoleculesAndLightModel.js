75ebf77ff946615a61cc81072bf3fc616774d744
Jesse <jesse.greenberg@colorado.edu>
2021-04-22 10:52:05 -0400

    Files from molecules-and-light moved under the micro screen, see https://github.com/phetsims/greenhouse-effect/issues/19

--- js/moleculesandlight/model/MoleculesAndLightModel.js
+++ js/moleculesandlight/model/MoleculesAndLightModel.js
@@ -19,18 +19,6 @@ class MoleculesAndLightModel extends PhotonAbsorptionModel {
 
     const initialTarget = MoleculesAndLightQueryParameters.openSciEd ? PhotonTarget.SINGLE_N2_MOLECULE : PhotonTarget.SINGLE_CO_MOLECULE;
     super( initialTarget, tandem );
-
-    // Clear all photons to avoid cases where photons of the previous wavelength
-    // could be absorbed after new wavelength was selected. Some users interpreted
-    // absorption of the previous wavelength as absorption of the selected wavelength
-    this.photonWavelengthProperty.lazyLink( () => {
-      this.resetPhotons();
-
-      // after clearing, next photon should be emitted right away
-      if ( this.photonEmitterOnProperty.get() ) {
-        this.setEmissionTimerToInitialCountdown();
-      }
-    } );
   }
 }
 
