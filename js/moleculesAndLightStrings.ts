95a67aefdf8db2473adc02f0ab9d9a194e9e1944
Sam Reid <reids@colorado.edu>
2022-05-07 09:34:13 -0600

    Use semicolons to terminate fields in type StringType, see https://github.com/phetsims/chipper/issues/1208

--- js/moleculesAndLightStrings.ts
+++ js/moleculesAndLightStrings.ts
@@ -9,179 +9,179 @@ import moleculesAndLight from './moleculesAndLight.js';
 
 type StringsType = {
   'molecules-and-light': {
-    'title': string
-  },
+    'title': string;
+  };
   'QuadWavelengthSelector': {
-    'Microwave': string,
-    'Infrared': string,
-    'Visible': string,
-    'Ultraviolet': string,
-    'HigherEnergy': string
-  },
-  'molecularNamePattern': string,
+    'Microwave': string;
+    'Infrared': string;
+    'Visible': string;
+    'Ultraviolet': string;
+    'HigherEnergy': string;
+  };
+  'molecularNamePattern': string;
   'ControlPanel': {
-    'CarbonMonoxide': string,
-    'Nitrogen': string,
-    'Oxygen': string,
-    'CarbonDioxide': string,
-    'Methane': string,
-    'NitrogenDioxide': string,
-    'Ozone': string,
-    'Water': string
-  },
+    'CarbonMonoxide': string;
+    'Nitrogen': string;
+    'Oxygen': string;
+    'CarbonDioxide': string;
+    'Methane': string;
+    'NitrogenDioxide': string;
+    'Ozone': string;
+    'Water': string;
+  };
   'ButtonNode': {
-    'ReturnMolecule': string
-  },
+    'ReturnMolecule': string;
+  };
   'SpectrumWindow': {
-    'buttonCaption': string,
-    'title': string,
-    'frequencyArrowLabel': string,
-    'wavelengthArrowLabel': string,
-    'radioBandLabel': string,
-    'microwaveBandLabel': string,
-    'infraredBandLabel': string,
-    'ultravioletBandLabel': string,
-    'xrayBandLabel': string,
-    'gammaRayBandLabel': string,
-    'visibleBandLabel': string,
-    'cyclesPerSecondUnits': string,
-    'metersUnits': string
-  },
+    'buttonCaption': string;
+    'title': string;
+    'frequencyArrowLabel': string;
+    'wavelengthArrowLabel': string;
+    'radioBandLabel': string;
+    'microwaveBandLabel': string;
+    'infraredBandLabel': string;
+    'ultravioletBandLabel': string;
+    'xrayBandLabel': string;
+    'gammaRayBandLabel': string;
+    'visibleBandLabel': string;
+    'cyclesPerSecondUnits': string;
+    'metersUnits': string;
+  };
   'keyboardHelpDialog': {
-    'observationWindow': string,
-    'turnLightSourceOnOrOff': string,
-    'pauseOrPlayShortcut': string,
-    'stepForwardShortcut': string
-  },
+    'observationWindow': string;
+    'turnLightSourceOnOrOff': string;
+    'pauseOrPlayShortcut': string;
+    'stepForwardShortcut': string;
+  };
   'openSciEd': {
-    'energySource': string
-  },
+    'energySource': string;
+  };
   'a11y': {
-    'playAreaSummary': string,
-    'controlAreaSummary': string,
-    'dynamicPlayingScreenSummaryPattern': string,
-    'dynamicPausedScreenSummaryPattern': string,
-    'simIsPaused': string,
-    'simIsPausedOnSlowSpeed': string,
-    'screenSummaryWithHintPattern': string,
-    'interactionHint': string,
+    'playAreaSummary': string;
+    'controlAreaSummary': string;
+    'dynamicPlayingScreenSummaryPattern': string;
+    'dynamicPausedScreenSummaryPattern': string;
+    'simIsPaused': string;
+    'simIsPausedOnSlowSpeed': string;
+    'screenSummaryWithHintPattern': string;
+    'interactionHint': string;
     'lightSource': {
-      'buttonLabelPattern': string,
-      'buttonPressedHelpText': string,
-      'buttonUnpressedHelpText': string
-    },
-    'lightSources': string,
-    'isOffAndPoints': string,
-    'emitsPhotons': string,
-    'emitsPhotonsOnSlowSpeed': string,
-    'observationWindowLabel': string,
-    'photonEmitterOffDescriptionPattern': string,
-    'inactiveAndPassesPhaseDescriptionPattern': string,
-    'targetMoleculePattern': string,
-    'absorptionPhaseBondsDescriptionPattern': string,
-    'absorptionPhaseMoleculeDescriptionPattern': string,
-    'breakApartPhaseDescriptionPattern': string,
-    'emissionPhaseDescriptionPattern': string,
-    'bendUpAndDown': string,
-    'stretchBackAndForth': string,
-    'rotatesClockwise': string,
-    'rotatesCounterClockwise': string,
-    'glowsString': string,
-    'breaksApart': string,
-    'emptySpace': string,
-    'lightSourceRadioButtonHelpText': string,
-    'molecules': string,
-    'moleculesRadioButtonHelpText': string,
-    'moleculeButtonLabelPattern': string,
-    'spectrumButtonLabel': string,
-    'spectrumButtonDescription': string,
-    'spectrumWindowDescription': string,
-    'spectrumWindowEnergyDescription': string,
-    'spectrumWindowSinWaveDescription': string,
-    'spectrumWindowLabelledSpectrumLabel': string,
-    'spectrumWindowLabelledSpectrumDescription': string,
-    'spectrumWindowLabelledSpectrumRadioLabel': string,
-    'spectrumWindowLabelledSpectrumMicrowaveLabel': string,
-    'spectrumWindowLabelledSpectrumInfraredLabel': string,
-    'spectrumWindowLabelledSpectrumVisibleLabel': string,
-    'spectrumWindowLabelledSpectrumUltravioletLabel': string,
-    'spectrumWindowLabelledSpectrumXRayLabel': string,
-    'spectrumWindowLabelledSpectrumGammaRayLabel': string,
-    'spectrumWindowLabelledSpectrumRadioFrequencyDescription': string,
-    'spectrumWindowLabelledSpectrumRadioWavelengthDescription': string,
-    'spectrumWindowLabelledSpectrumMicrowaveFrequencyDescription': string,
-    'spectrumWindowLabelledSpectrumMicrowaveWavelengthDescription': string,
-    'spectrumWindowLabelledSpectrumInfraredFrequencyDescription': string,
-    'spectrumWindowLabelledSpectrumInfraredWavelengthDescription': string,
-    'spectrumWindowLabelledSpectrumVisibleFrequencyDescription': string,
-    'spectrumWindowLabelledSpectrumVisibleWavelengthDescription': string,
-    'spectrumWindowLabelledSpectrumVisibleGraphicalDescription': string,
-    'spectrumWindowLabelledSpectrumUltravioletFrequencyDescription': string,
-    'spectrumWindowLabelledSpectrumUltravioletWavelengthDescription': string,
-    'spectrumWindowLabelledSpectrumXRayFrequencyDescription': string,
-    'spectrumWindowLabelledSpectrumXRayWavelengthDescription': string,
-    'spectrumWindowLabelledSpectrumGammaRayFrequencyDescription': string,
-    'spectrumWindowLabelledSpectrumGammaRayWavelengthDescription': string,
-    'carbonMonoxide': string,
-    'nitrogen': string,
-    'oxygen': string,
-    'diatomicOxygen': string,
-    'carbonDioxide': string,
-    'nitrogenDioxide': string,
-    'water': string,
-    'ozone': string,
-    'methane': string,
-    'linear': string,
-    'bent': string,
-    'tetrahedral': string,
-    'diatomic': string,
-    'geometryLabelPattern': string,
-    'linearGeometryDescription': string,
-    'bentGeometryDescription': string,
-    'tetrahedralGeometryDescription': string,
-    'shortStretchingAlert': string,
-    'longStretchingAlert': string,
-    'shortBendingAlert': string,
-    'longBendingAlert': string,
-    'shortRotatingAlert': string,
-    'longRotatingAlert': string,
-    'shortGlowingAlert': string,
-    'longGlowingAlert': string,
-    'breaksApartAlertPattern': string,
-    'pausedEmittingPattern': string,
-    'pausedPassingPattern': string,
-    'slowMotionPassingPattern': string,
-    'photonPasses': string,
-    'photonsPassing': string,
-    'slowMotionVibratingPattern': string,
-    'slowMotionAbsorbedMoleculeExcitedPattern': string,
-    'slowMotionAbsorbedShortPattern': string,
-    'slowMotionBreakApartPattern': string,
-    'moleculesFloatingAwayPattern': string,
-    'breakApartDescriptionWithFloatPattern': string,
-    'moleculePiecesGone': string,
-    'slowMotionEmittedPattern': string,
-    'resetOrChangeMolecule': string,
+      'buttonLabelPattern': string;
+      'buttonPressedHelpText': string;
+      'buttonUnpressedHelpText': string;
+    };
+    'lightSources': string;
+    'isOffAndPoints': string;
+    'emitsPhotons': string;
+    'emitsPhotonsOnSlowSpeed': string;
+    'observationWindowLabel': string;
+    'photonEmitterOffDescriptionPattern': string;
+    'inactiveAndPassesPhaseDescriptionPattern': string;
+    'targetMoleculePattern': string;
+    'absorptionPhaseBondsDescriptionPattern': string;
+    'absorptionPhaseMoleculeDescriptionPattern': string;
+    'breakApartPhaseDescriptionPattern': string;
+    'emissionPhaseDescriptionPattern': string;
+    'bendUpAndDown': string;
+    'stretchBackAndForth': string;
+    'rotatesClockwise': string;
+    'rotatesCounterClockwise': string;
+    'glowsString': string;
+    'breaksApart': string;
+    'emptySpace': string;
+    'lightSourceRadioButtonHelpText': string;
+    'molecules': string;
+    'moleculesRadioButtonHelpText': string;
+    'moleculeButtonLabelPattern': string;
+    'spectrumButtonLabel': string;
+    'spectrumButtonDescription': string;
+    'spectrumWindowDescription': string;
+    'spectrumWindowEnergyDescription': string;
+    'spectrumWindowSinWaveDescription': string;
+    'spectrumWindowLabelledSpectrumLabel': string;
+    'spectrumWindowLabelledSpectrumDescription': string;
+    'spectrumWindowLabelledSpectrumRadioLabel': string;
+    'spectrumWindowLabelledSpectrumMicrowaveLabel': string;
+    'spectrumWindowLabelledSpectrumInfraredLabel': string;
+    'spectrumWindowLabelledSpectrumVisibleLabel': string;
+    'spectrumWindowLabelledSpectrumUltravioletLabel': string;
+    'spectrumWindowLabelledSpectrumXRayLabel': string;
+    'spectrumWindowLabelledSpectrumGammaRayLabel': string;
+    'spectrumWindowLabelledSpectrumRadioFrequencyDescription': string;
+    'spectrumWindowLabelledSpectrumRadioWavelengthDescription': string;
+    'spectrumWindowLabelledSpectrumMicrowaveFrequencyDescription': string;
+    'spectrumWindowLabelledSpectrumMicrowaveWavelengthDescription': string;
+    'spectrumWindowLabelledSpectrumInfraredFrequencyDescription': string;
+    'spectrumWindowLabelledSpectrumInfraredWavelengthDescription': string;
+    'spectrumWindowLabelledSpectrumVisibleFrequencyDescription': string;
+    'spectrumWindowLabelledSpectrumVisibleWavelengthDescription': string;
+    'spectrumWindowLabelledSpectrumVisibleGraphicalDescription': string;
+    'spectrumWindowLabelledSpectrumUltravioletFrequencyDescription': string;
+    'spectrumWindowLabelledSpectrumUltravioletWavelengthDescription': string;
+    'spectrumWindowLabelledSpectrumXRayFrequencyDescription': string;
+    'spectrumWindowLabelledSpectrumXRayWavelengthDescription': string;
+    'spectrumWindowLabelledSpectrumGammaRayFrequencyDescription': string;
+    'spectrumWindowLabelledSpectrumGammaRayWavelengthDescription': string;
+    'carbonMonoxide': string;
+    'nitrogen': string;
+    'oxygen': string;
+    'diatomicOxygen': string;
+    'carbonDioxide': string;
+    'nitrogenDioxide': string;
+    'water': string;
+    'ozone': string;
+    'methane': string;
+    'linear': string;
+    'bent': string;
+    'tetrahedral': string;
+    'diatomic': string;
+    'geometryLabelPattern': string;
+    'linearGeometryDescription': string;
+    'bentGeometryDescription': string;
+    'tetrahedralGeometryDescription': string;
+    'shortStretchingAlert': string;
+    'longStretchingAlert': string;
+    'shortBendingAlert': string;
+    'longBendingAlert': string;
+    'shortRotatingAlert': string;
+    'longRotatingAlert': string;
+    'shortGlowingAlert': string;
+    'longGlowingAlert': string;
+    'breaksApartAlertPattern': string;
+    'pausedEmittingPattern': string;
+    'pausedPassingPattern': string;
+    'slowMotionPassingPattern': string;
+    'photonPasses': string;
+    'photonsPassing': string;
+    'slowMotionVibratingPattern': string;
+    'slowMotionAbsorbedMoleculeExcitedPattern': string;
+    'slowMotionAbsorbedShortPattern': string;
+    'slowMotionBreakApartPattern': string;
+    'moleculesFloatingAwayPattern': string;
+    'breakApartDescriptionWithFloatPattern': string;
+    'moleculePiecesGone': string;
+    'slowMotionEmittedPattern': string;
+    'resetOrChangeMolecule': string;
     'photonEmitter': {
       'alerts': {
-        'photonsOff': string,
-        'photonsOn': string,
-        'photonsOnSlowSpeed': string,
-        'photonsOnSimPaused': string,
-        'photonsOnSlowSpeedSimPaused': string,
-        'pausedPhotonEmittedPattern': string
+        'photonsOff': string;
+        'photonsOn': string;
+        'photonsOnSlowSpeed': string;
+        'photonsOnSimPaused': string;
+        'photonsOnSlowSpeedSimPaused': string;
+        'pausedPhotonEmittedPattern': string;
       }
-    },
+    };
     'timeControls': {
-      'simPausedEmitterOnAlert': string,
-      'simPausedEmitterOffAlert': string,
-      'simPlayingHintAlert': string,
-      'stepHintAlert': string
-    },
+      'simPausedEmitterOnAlert': string;
+      'simPausedEmitterOffAlert': string;
+      'simPlayingHintAlert': string;
+      'stepHintAlert': string;
+    };
     'keyboardHelpDialog': {
-      'turnLightSourceOnOrOffDescription': string,
-      'pauseOrPlayShortcutDescription': string,
-      'stepForwardShortcutDescription': string
+      'turnLightSourceOnOrOffDescription': string;
+      'pauseOrPlayShortcutDescription': string;
+      'stepForwardShortcutDescription': string;
     }
   }
 };
