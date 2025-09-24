d51287ce263b67c958ffc6424db7482b14571291
phet-dev <phetadmin@gmail.com>
2025-09-24 03:57:49 -0600

    update copyright dates from daily grunt work

--- js/common/view/MoleculesAndLightKeyboardHelpContent.ts
+++ js/common/view/MoleculesAndLightKeyboardHelpContent.ts
@@ -1,4 +1,4 @@
-// Copyright 2017-2024, University of Colorado Boulder
+// Copyright 2017-2025, University of Colorado Boulder
 
 /**
  * Content for the "Keyboard Shortcuts" dialog that can be brought up from the sim navigation bar.
